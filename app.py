4de66a2941c19e5ee14082cb5d2f706cf4d97a21
Ans Hussain <i201824@nu.edu.pk>
2024-01-10 12:43:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
+# app.py
 from flask import Flask, render_template, request
+import numpy as np
 import joblib
 
 app = Flask(__name__)
 
-# Load the trained model and scaler
-model = joblib.load('iris_model.joblib')
-scaler = joblib.load('iris_scaler.joblib')
+# Load the trained model
+model = joblib.load('decision_tree_model.joblib')
+
+# Define the feature names
+feature_names = ['shop_id', 'item_id',
+                 'date_block_num', 'item_category_id', 'Month', 'Year']
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    # Pass the feature names to the template
+    return render_template('index.html', feature_names=feature_names)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        sepal_length = float(request.form['sepal_length'])
-        sepal_width = float(request.form['sepal_width'])
-        petal_length = float(request.form['petal_length'])
-        petal_width = float(request.form['petal_width'])
+        # # Assuming the form input names match the feature names
+        # features = [float(request.form[name]) for name in feature_names]
+        # input_data = pd.DataFrame([features], columns=feature_names)
+        # prediction = model.predict(input_data)
 
-        # Normalize input features
-        input_features = scaler.transform(
-            [[sepal_length, sepal_width, petal_length, petal_width]])
-
+        shop_id = int(request.form['shop_id'])
+        item_id = int(request.form['item_id'])
+        date_block_num = int(request.form['date_block_num'])
+        item_category_id = int(request.form['item_category_id'])
+        Month = int(request.form['Month'])
+        Year = int(request.form['Year'])
+        input_features = np.array([
+            shop_id, item_id, date_block_num, item_category_id, Month, Year])
         # Make prediction
-        prediction = model.predict(input_features)[0]
-
+        input_features = input_features.reshape(1,-1)
+        prediction = model.predict(input_features)
         return render_template('index.html', prediction=prediction)
 
 
